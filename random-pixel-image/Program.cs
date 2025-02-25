e2e5ac7a40493a4598705f93c5f583dca9b7cf4b
Lino Meyer <lino.meyer@stud.hslu.ch>
2025-02-25 15:34:29 +0100

    init repo

---
+++ random-pixel-image/Program.cs
@@ -0,0 +1,43 @@
+﻿using SkiaSharp;
+
+namespace random_pixel_image;
+
+internal static class Program
+{
+    private const string FilePath = "skia_random_colors.png";
+    private const int Width = 300;
+    private const int Height = 300;
+
+    private static void Main()
+    {
+        var bitmap = GenerateBitmap();
+        SaveBitmapAsImage(bitmap);
+        Console.WriteLine($"Random color image saved to {FilePath}");
+    }
+
+    private static SKBitmap GenerateBitmap()
+    {
+        var random = new Random();
+        var bitmap = new SKBitmap(Width, Height);
+
+        for (var y = 0; y < Height; y++)
+            for (var x = 0; x < Width; x++)
+            {
+                var color = new SKColor(
+                    (byte)random.Next(256), // Random Red
+                    (byte)random.Next(256), // Random Green
+                    (byte)random.Next(256), // Random Blue
+                    255 // Full opacity
+                );
+                bitmap.SetPixel(x, y, color);
+            }
+
+        return bitmap;
+    }
+
+    private static void SaveBitmapAsImage(SKBitmap bitmap)
+    {
+        using var fs = new FileStream(FilePath, FileMode.Create);
+        bitmap.Encode(fs, SKEncodedImageFormat.Png, 100);
+    }
+}
